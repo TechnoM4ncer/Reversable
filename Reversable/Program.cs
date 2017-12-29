c1bb589acce8468abb21e11f4b4e1f973b0173f3
Kino Kashi <kinodekirakashi@live.com>
2017-12-29 14:28:09 -0600

    First commit

---
+++ Reversable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Reversable
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        { 
+            Start:
+            String revA = System.String.Empty;
+            String revB = System.String.Empty;
+
+            Console.WriteLine("Please enter a string to be reversed");
+
+            revA = Console.ReadLine();
+
+            for (int i = revA.Length - 1; i <= revA.Length && i >= 0; i--)
+            {
+                revB += revA[i];
+            }
+            Clipboard.SetText(revB);
+            Console.WriteLine(revA + " reversed is " + revB + " and has been copied to your clipboard.");
+
+            goto Start;
+
+        }
+    }
+}
